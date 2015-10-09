c23e3340a37e1d8954901f259f8d6667b34eb857
MoistBaguette <MoistBaguette@users.noreply.github.com>
2015-10-09 17:24:37 +0200

    Use PBOConsole to build PBO for dev purposes

--- make.sh
+++ make.sh
@@ -5,3 +5,4 @@ mkdir -p build/FP_Static_Weapons            # Create Empty Build Folder
 cp -r src/* build/FP_Static_Weapons/        # Copy over all source files
 rm -rf build/FP_Static_Weapons/*/includes   # Remove all include files
 grunt                                       # Generate the config.cpp file.
+PBOConsole -pack build/FP_Static_Weapons build/FP_Static_Weapons.pbo

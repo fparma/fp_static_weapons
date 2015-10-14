bae7ec794984c5683c9cbad70cfbc877fab9db32
MoistBaguette <MoistBaguette@users.noreply.github.com>
2015-10-14 18:12:19 +0200

    Remove .wav files before making the PBO. Don't need to be in there.

--- make.sh
+++ make.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/bash
 
-rm -rf build/FP_Static_Weapons              # Delete Build Folder
-mkdir -p build/FP_Static_Weapons            # Create Empty Build Folder
-cp -r src/* build/FP_Static_Weapons/        # Copy over all source files
-rm -rf build/FP_Static_Weapons/*/includes   # Remove all include files
-grunt                                       # Generate the config.cpp file.
+rm -rf build/FP_Static_Weapons                                          # Delete Build Folder
+mkdir -p build/FP_Static_Weapons                                        # Create Empty Build Folder
+cp -r src/* build/FP_Static_Weapons/                                    # Copy over all source files
+rm -rf build/FP_Static_Weapons/*/includes                               # Remove all include files
+rm -rf build/FP_Static_Weapons/*/sounds/*.wav                           # Remove all .wav sound files
+grunt                                                                   # Generate the config.cpp file.
 PBOConsole -pack build/FP_Static_Weapons build/FP_Static_Weapons.pbo

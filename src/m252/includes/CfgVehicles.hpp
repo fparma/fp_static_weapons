26cdc7bd45a4b57cbd4abaa9f78145f923b978d6
MoistBaguette <MoistBaguette@users.noreply.github.com>
2015-10-22 17:05:51 +0200

    Added Icon to M252 Mortar

--- src/m252/includes/CfgVehicles.hpp
+++ src/m252/includes/CfgVehicles.hpp
@@ -1,8 +1,8 @@
 class fp_static_m252_base : B_Mortar_01_F {
   displayName = "81mm M252 Mortar";
   model = "\FP_Static_Weapons\m252\81Mortar";
-  picture = "\FP_Static_Weapons\m252\icon\podnos_2b14_ca.paa";
-  icon = "\FP_Static_Weapons\m252\icon\icomap_podnos_2b14_ca.paa";
+  picture = "\FP_Static_Weapons\m252\icon\mortar_m251_ca.paa";
+  icon = "\FP_Static_Weapons\m252\icon\icomap_mortar_m251_ca.paa";
 };
 
 class fp_static_m252_b : fp_static_m252_base {

1057f3e87019b31d8960710751cd72f5b0a658b3
anon <anon@anonmail.com>
2016-02-17 20:52:18 +0100

    Another missing ;

--- src/tow/includes/CfgWeapons.hpp
+++ src/tow/includes/CfgWeapons.hpp
@@ -5,6 +5,6 @@ class fp_static_weap_tow : missiles_titan {
   class StandardSound {
     begin1[] = {"\FP_Static_Weapons\tow\sounds\tow_fire_3p",1.41254,1,1100};
     soundBegin[] = {"begin1",1};
-  }
+  };
   magazines[] = {"fp_static_mag_tow"};
 };

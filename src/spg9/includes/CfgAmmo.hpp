6eaa72d2eb208d96d2e0d55ff1fe2a1a908d2548
anon <anon@anonmail.com>
2016-02-20 22:54:31 +0100

    Adjusted tabs to spaces

--- src/spg9/includes/CfgAmmo.hpp
+++ src/spg9/includes/CfgAmmo.hpp
@@ -1,7 +1,7 @@
 class fp_static_ammo_pg9v : RocketBase {
-	hit = 320;
-	indirectHit = 20;
-	indirectHitRange = 1.2;
+  hit = 320;
+  indirectHit = 20;
+  indirectHitRange = 1.2;
   maxSpeed = 800;
   typicalSpeed = 700;
   fuseDistance = 12;
@@ -9,13 +9,13 @@ class fp_static_ammo_pg9v : RocketBase {
   thrustTime = 1;
   thrust = 500;
   explosive = 0.8;
-	cost = 500;
-	model = "\A3\Weapons_f\Data\bullettracer\shell_tracer_white";
+  cost = 500;
+  model = "\A3\Weapons_f\Data\bullettracer\shell_tracer_white";
   effectsmissileinit = "RocketBackEffectsStaticRPG";
   effectsMissile = "missile2";
 };
 class fp_static_ammo_og9v : fp_static_ammo_pg9v {
-	hit = 140;
-	indirectHit = 45;
-	indirectHitRange = 10;
+  hit = 140;
+  indirectHit = 45;
+  indirectHitRange = 10;
 };

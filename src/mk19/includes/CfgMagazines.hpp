44ca44eb0d1df63b2df3ed90d02496bd33bb37a4
anon <anon@anonmail.com>
2016-02-21 16:19:59 +0100

    Use 20mm for now. Doesent fall over

--- src/mk19/includes/CfgMagazines.hpp
+++ src/mk19/includes/CfgMagazines.hpp
@@ -1,7 +1,7 @@
 class fp_static_mag_48rnd_mk19: VehicleMagazine {
   scope = 2;
   displayName = "Mk. 19 40mm";
-  ammo = "G_40mm_HE";
+  ammo = "G_20mm_HE";
   count = 48;
   initSpeed = 240;
   maxLeadSpeed = 100;

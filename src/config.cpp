66b42fdf5fdf52951a8ef85c685f43897f503fb8
anon <anon@anonmail.com>
2016-02-22 22:24:24 +0100

    Use Preprocessor includes

--- src/config.cpp
+++ src/config.cpp
@@ -1,4 +1,3 @@
-{#/* Constant Declaration*/#}
 {%- set SIDE = {
   "NONE" : -1,
   "EAST" : 0,
@@ -62,7 +61,6 @@ class CfgPatches {
   };
 };
 
-{#/* External Class References */#}
 class Mode_SemiAuto;
 
 class CfgMovesBasic {
@@ -79,8 +77,8 @@ class CfgMovesBasic {
     {% if weapons.d30 %}D30_Commander = "D30_Commander";D30_Gunner = "D30_Gunner";D30_Cargo = "D30_Cargo";{% endif %}
     {% if weapons.metis %}Metis_Gunner = "Metis_Gunner";{% endif %}
     {% if weapons.mk19 %}MK19_Gunner = "MK19_Gunner";{% endif %}
-    {% if weapons.m252 %}{% endif %} {#/* Uses Default MK6Mortar Animations */#}
-    {% if weapons.podnos %}{% endif %} {#/* Uses Default MK6Mortar Animations */#}
+    {% if weapons.m252 %}{% endif %}
+    {% if weapons.podnos %}{% endif %}
     {% if weapons.search %}searchlight_Gunner = "searchlight_Gunner";{% endif %}
     {% if weapons.igla %}Igla_Gunner = "Igla_Gunner";{% endif %}
   };
@@ -99,21 +97,21 @@ class CfgMovesMaleSdr : CfgMovesBasic {
       righHandIKEnd = 1;
     };
 
-    {% if weapons.m2 %}{% include "src/m2/includes/CfgMovesMaleSdr.hpp" %}{% endif %}
-    {% if weapons.dshkm %}{% include "src/dshkm/includes/CfgMovesMaleSdr.hpp" %}{% endif %}
-    {% if weapons.spg9 %}{% include "src/spg9/includes/CfgMovesMaleSdr.hpp" %}{% endif %}
-    {% if weapons.zu23 %}{% include "src/zu23/includes/CfgMovesMaleSdr.hpp" %}{% endif %}
-    {% if weapons.m119 %}{% include "src/m119/includes/CfgMovesMaleSdr.hpp" %}{% endif %}
-    {% if weapons.tow %}{% include "src/tow/includes/CfgMovesMaleSdr.hpp" %}{% endif %}
-    {% if weapons.mk19 %}{% include "src/mk19/includes/CfgMovesMaleSdr.hpp" %}{% endif %}
-    {% if weapons.ags30 %}{% include "src/ags30/includes/CfgMovesMaleSdr.hpp" %}{% endif %}
-    {% if weapons.kord %}{% include "src/kord/includes/CfgMovesMaleSdr.hpp" %}{% endif %}
-    {% if weapons.d30 %}{% include "src/d30/includes/CfgMovesMaleSdr.hpp" %}{% endif %}
-    {% if weapons.metis %}{% include "src/metis/includes/CfgMovesMaleSdr.hpp" %}{% endif %}
-    {% if weapons.m252 %}{% include "src/m252/includes/CfgMovesMaleSdr.hpp" %}{% endif %}
-    {% if weapons.podnos %}{% include "src/podnos/includes/CfgMovesMaleSdr.hpp" %}{% endif %}
-    {% if weapons.igla %}{% include "src/igla/includes/CfgMovesMaleSdr.hpp" %}{% endif %}
-    {% if weapons.search %}{% include "src/searchlight/includes/CfgMovesMaleSdr.hpp" %}{% endif %}
+    #include "m2\includes\CfgMovesMaleSdr.hpp"
+    #include "dshkm\includes\CfgMovesMaleSdr.hpp"
+    #include "spg9\includes\CfgMovesMaleSdr.hpp"
+    #include "zu23\includes\CfgMovesMaleSdr.hpp"
+    #include "m119\includes\CfgMovesMaleSdr.hpp"
+    #include "tow\includes\CfgMovesMaleSdr.hpp"
+    #include "mk19\includes\CfgMovesMaleSdr.hpp"
+    #include "ags30\includes\CfgMovesMaleSdr.hpp"
+    #include "kord\includes\CfgMovesMaleSdr.hpp"
+    #include "d30\includes\CfgMovesMaleSdr.hpp"
+    #include "metis\includes\CfgMovesMaleSdr.hpp"
+    #include "m252\includes\CfgMovesMaleSdr.hpp"
+    #include "podnos\includes\CfgMovesMaleSdr.hpp"
+    #include "igla\includes\CfgMovesMaleSdr.hpp"
+    #include "searchlight\includes\CfgMovesMaleSdr.hpp"
   };
 };
 
@@ -139,21 +137,22 @@ class CfgVehicles {
   class Mortar_01_base_F : StaticMortar{};
   class B_Mortar_01_F : Mortar_01_base_F{};
 
-  {% if weapons.m2 %}{% include "src/m2/includes/CfgVehicles.hpp" %}{% endif %}
-  {% if weapons.dshkm %}{% include "src/dshkm/includes/CfgVehicles.hpp" %}{% endif %}
-  {% if weapons.spg9 %}{% include "src/spg9/includes/CfgVehicles.hpp" %}{% endif %}
-  {% if weapons.zu23 %}{% include "src/zu23/includes/CfgVehicles.hpp" %}{% endif %}
-  {% if weapons.m119 %}{% include "src/m119/includes/CfgVehicles.hpp" %}{% endif %}
-  {% if weapons.tow %}{% include "src/tow/includes/CfgVehicles.hpp" %}{% endif %}
-  {% if weapons.mk19 %}{% include "src/mk19/includes/CfgVehicles.hpp" %}{% endif %}
-  {% if weapons.ags30 %}{% include "src/ags30/includes/CfgVehicles.hpp" %}{% endif %}
-  {% if weapons.kord %}{% include "src/kord/includes/CfgVehicles.hpp" %}{% endif %}
-  {% if weapons.d30 %}{% include "src/d30/includes/CfgVehicles.hpp" %}{% endif %}
-  {% if weapons.metis %}{% include "src/metis/includes/CfgVehicles.hpp" %}{% endif %}
-  {% if weapons.m252 %}{% include "src/m252/includes/CfgVehicles.hpp" %}{% endif %}
-  {% if weapons.podnos %}{% include "src/podnos/includes/CfgVehicles.hpp" %}{% endif %}
-  {% if weapons.igla %}{% include "src/igla/includes/CfgVehicles.hpp" %}{% endif %}
-  {% if weapons.search %}{% include "src/searchlight/includes/CfgVehicles.hpp" %}{% endif %}
+  #include "m2\includes\CfgVehicles.hpp"
+  #include "dshkm\includes\CfgVehicles.hpp"
+  #include "spg9\includes\CfgVehicles.hpp"
+  #include "zu23\includes\CfgVehicles.hpp"
+  #include "m119\includes\CfgVehicles.hpp"
+  #include "tow\includes\CfgVehicles.hpp"
+  #include "mk19\includes\CfgVehicles.hpp"
+  #include "ags30\includes\CfgVehicles.hpp"
+  #include "kord\includes\CfgVehicles.hpp"
+  #include "d30\includes\CfgVehicles.hpp"
+  #include "metis\includes\CfgVehicles.hpp"
+  #include "m252\includes\CfgVehicles.hpp"
+  #include "podnos\includes\CfgVehicles.hpp"
+  #include "igla\includes\CfgVehicles.hpp"
+  #include "searchlight\includes\CfgVehicles.hpp"
+
 };
 
 class CfgWeapons {
@@ -174,20 +173,20 @@ class CfgWeapons {
   class mortar_155mm_AMOS : CannonCore{};
   class missiles_titan : MissileLauncher{};
 
-  {% if weapons.m2 %}{% include "src/m2/includes/CfgWeapons.hpp" %}{% endif %}
-  {% if weapons.dshkm %}{% include "src/dshkm/includes/CfgWeapons.hpp" %}{% endif %}
-  {% if weapons.spg9 %}{% include "src/spg9/includes/CfgWeapons.hpp" %}{% endif %}
-  {% if weapons.zu23 %}{% include "src/zu23/includes/CfgWeapons.hpp" %}{% endif %}
-  {% if weapons.m119 %}{% include "src/m119/includes/CfgWeapons.hpp" %}{% endif %}
-  {% if weapons.tow %}{% include "src/tow/includes/CfgWeapons.hpp" %}{% endif %}
-  {% if weapons.mk19 %}{% include "src/mk19/includes/CfgWeapons.hpp" %}{% endif %}
-  {% if weapons.ags30 %}{% include "src/ags30/includes/CfgWeapons.hpp" %}{% endif %}
-  {% if weapons.kord %}{% include "src/kord/includes/CfgWeapons.hpp" %}{% endif %}
-  {% if weapons.d30 %}{% include "src/d30/includes/CfgWeapons.hpp" %}{% endif %}
-  {% if weapons.metis %}{% include "src/metis/includes/CfgWeapons.hpp" %}{% endif %}
-  {% if weapons.m252 %}{% include "src/m252/includes/CfgWeapons.hpp" %}{% endif %}
-  {% if weapons.podnos %}{% include "src/podnos/includes/CfgWeapons.hpp" %}{% endif %}
-  {% if weapons.igla %}{% include "src/igla/includes/CfgWeapons.hpp" %}{% endif %}
+  #include "m2\includes\CfgWeapons.hpp"
+  #include "dshkm\includes\CfgWeapons.hpp"
+  #include "spg9\includes\CfgWeapons.hpp"
+  #include "zu23\includes\CfgWeapons.hpp"
+  #include "m119\includes\CfgWeapons.hpp"
+  #include "tow\includes\CfgWeapons.hpp"
+  #include "mk19\includes\CfgWeapons.hpp"
+  #include "ags30\includes\CfgWeapons.hpp"
+  #include "kord\includes\CfgWeapons.hpp"
+  #include "d30\includes\CfgWeapons.hpp"
+  #include "metis\includes\CfgWeapons.hpp"
+  #include "m252\includes\CfgWeapons.hpp"
+  #include "podnos\includes\CfgWeapons.hpp"
+  #include "igla\includes\CfgWeapons.hpp"
 };
 
 class CfgMagazines {
@@ -196,20 +195,20 @@ class CfgMagazines {
   class CA_LauncherMagazine : CA_Magazine{};
   class VehicleMagazine : CA_Magazine{};
 
-  {% if weapons.m2 %}{% include "src/m2/includes/CfgMagazines.hpp" %}{% endif %}
-  {% if weapons.dshkm %}{% include "src/dshkm/includes/CfgMagazines.hpp" %}{% endif %}
-  {% if weapons.spg9 %}{% include "src/spg9/includes/CfgMagazines.hpp" %}{% endif %}
-  {% if weapons.zu23 %}{% include "src/zu23/includes/CfgMagazines.hpp" %}{% endif %}
-  {% if weapons.m119 %}{% include "src/m119/includes/CfgMagazines.hpp" %}{% endif %}
-  {% if weapons.tow %}{% include "src/tow/includes/CfgMagazines.hpp" %}{% endif %}
-  {% if weapons.mk19 %}{% include "src/mk19/includes/CfgMagazines.hpp" %}{% endif %}
-  {% if weapons.ags30 %}{% include "src/ags30/includes/CfgMagazines.hpp" %}{% endif %}
-  {% if weapons.kord %}{% include "src/kord/includes/CfgMagazines.hpp" %}{% endif %}
-  {% if weapons.d30 %}{% include "src/d30/includes/CfgMagazines.hpp" %}{% endif %}
-  {% if weapons.metis %}{% include "src/metis/includes/CfgMagazines.hpp" %}{% endif %}
-  {% if weapons.m252 %}{% include "src/m252/includes/CfgMagazines.hpp" %}{% endif %}
-  {% if weapons.podnos %}{% include "src/podnos/includes/CfgMagazines.hpp" %}{% endif %}
-  {% if weapons.igla %}{% include "src/igla/includes/CfgMagazines.hpp" %}{% endif %}
+  #include "m2\includes\CfgMagazines.hpp"
+  #include "dshkm\includes\CfgMagazines.hpp"
+  #include "spg9\includes\CfgMagazines.hpp"
+  #include "zu23\includes\CfgMagazines.hpp"
+  #include "m119\includes\CfgMagazines.hpp"
+  #include "tow\includes\CfgMagazines.hpp"
+  #include "mk19\includes\CfgMagazines.hpp"
+  #include "ags30\includes\CfgMagazines.hpp"
+  #include "kord\includes\CfgMagazines.hpp"
+  #include "d30\includes\CfgMagazines.hpp"
+  #include "metis\includes\CfgMagazines.hpp"
+  #include "m252\includes\CfgMagazines.hpp"
+  #include "podnos\includes\CfgMagazines.hpp"
+  #include "igla\includes\CfgMagazines.hpp"
 };
 
 class CfgAmmo {
@@ -223,18 +222,18 @@ class CfgAmmo {
   class B_35mm_AA;
   class M_Titan_AT;
 
-  {% if weapons.m2 %}{% include "src/m2/includes/CfgAmmo.hpp" %}{% endif %}
-  {% if weapons.dshkm %}{% include "src/dshkm/includes/CfgAmmo.hpp" %}{% endif %}
-  {% if weapons.spg9 %}{% include "src/spg9/includes/CfgAmmo.hpp" %}{% endif %}
-  {% if weapons.zu23 %}{% include "src/zu23/includes/CfgAmmo.hpp" %}{% endif %}
-  {% if weapons.m119 %}{% include "src/m119/includes/CfgAmmo.hpp" %}{% endif %}
-  {% if weapons.tow %}{% include "src/tow/includes/CfgAmmo.hpp" %}{% endif %}
-  {% if weapons.mk19 %}{% include "src/mk19/includes/CfgAmmo.hpp" %}{% endif %}
-  {% if weapons.ags30 %}{% include "src/ags30/includes/CfgAmmo.hpp" %}{% endif %}
-  {% if weapons.kord %}{% include "src/kord/includes/CfgAmmo.hpp" %}{% endif %}
-  {% if weapons.d30 %}{% include "src/d30/includes/CfgAmmo.hpp" %}{% endif %}
-  {% if weapons.metis %}{% include "src/metis/includes/CfgAmmo.hpp" %}{% endif %}
-  {% if weapons.m252 %}{% include "src/m252/includes/CfgAmmo.hpp" %}{% endif %}
-  {% if weapons.podnos %}{% include "src/podnos/includes/CfgAmmo.hpp" %}{% endif %}
-  {% if weapons.igla %}{% include "src/igla/includes/CfgAmmo.hpp" %}{% endif %}
+  #include "m2\includes\CfgAmmo.hpp"
+  #include "dshkm\includes\CfgAmmo.hpp"
+  #include "spg9\includes\CfgAmmo.hpp"
+  #include "zu23\includes\CfgAmmo.hpp"
+  #include "m119\includes\CfgAmmo.hpp"
+  #include "tow\includes\CfgAmmo.hpp"
+  #include "mk19\includes\CfgAmmo.hpp"
+  #include "ags30\includes\CfgAmmo.hpp"
+  #include "kord\includes\CfgAmmo.hpp"
+  #include "d30\includes\CfgAmmo.hpp"
+  #include "metis\includes\CfgAmmo.hpp"
+  #include "m252\includes\CfgAmmo.hpp"
+  #include "podnos\includes\CfgAmmo.hpp"
+  #include "igla\includes\CfgAmmo.hpp"
 };

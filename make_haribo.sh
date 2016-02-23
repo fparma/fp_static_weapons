6e4e9cdfd6a2fb1f5c8260227636b73b498c4d70
LeipeHaribo112 <h_holsappel@live.nl>
2016-02-23 17:35:38 +0100

    added own makefile

---
+++ make_haribo.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+rm -rf p:/build/FP_Static_Weapons                                          # Delete Build Folder
+mkdir -p p:/build/FP_Static_Weapons                                        # Create Empty Build Folder
+cp -r src/* p:/build/FP_Static_Weapons/                                    # Copy over all source files
+rm -rf p:/build/FP_Static_Weapons/*/sounds/*.wav                           # Remove all .wav sound files
+rm -rf p:/build/FP_Static_Weapons/*.md                                     # Remove Weapon Specific Markdown Documentation
+#PBOConsole -pack build/FP_Static_Weapons build/FP_Static_Weapons.pbo    # Generate a PBO using PBOManager. TODO: Mikero's PBO Tools

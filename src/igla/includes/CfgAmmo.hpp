449cea092b3fcf2318ce38cb6079d11f15a953e5
anon <anon@anonmail.com>
2016-02-28 12:57:21 +0100

    Custom Igla Ammo. Reduced Damage of the missile

---
+++ src/igla/includes/CfgAmmo.hpp
@@ -0,0 +1,7 @@
+class M_Zephyr;
+class fp_static_ammo_igla : M_Zephyr {
+  cost = 1500;
+  hit = 200;
+  indirectHit = 45;
+  indirectHitRange = 8;
+};
--- src/igla/includes/CfgMagazines.hpp
+++ src/igla/includes/CfgMagazines.hpp
@@ -2,4 +2,5 @@ class 4Rnd_GAA_missiles;
 class fp_static_mag_igla : 4Rnd_GAA_missiles {
   displayName = "9M39";
   count = 2;
+  ammo = "fp_static_ammo_igla";
 };

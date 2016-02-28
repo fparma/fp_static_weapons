15486f93fa1c01eb8350dcc4297f2aaad6988546
anon <anon@anonmail.com>
2016-02-28 14:32:44 +0100

    Use Actual model for the Igla Missile

--- src/igla/includes/CfgAmmo.hpp
+++ src/igla/includes/CfgAmmo.hpp
@@ -4,4 +4,6 @@ class fp_static_ammo_igla : M_Zephyr {
   hit = 200;
   indirectHit = 45;
   indirectHitRange = 8;
+  model = "\CUP\Weapons\CUP_Weapons_Ammunition\9K38_Igla\CUP_9K38_Igla_fly.p3d";
+  ace_rearm_caliber = 250;
 };

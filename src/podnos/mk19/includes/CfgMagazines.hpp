f35f43d58e0d14d262cbecfaf623f972a6550bd7
anon <anon@anonmail.com>
2016-03-06 13:08:47 +0100

    How did this get in there?

--- src/podnos/mk19/includes/CfgMagazines.hpp
+++
@@ -1,9 +0,0 @@
-class fp_static_mag_48rnd_mk19: VehicleMagazine {
-  scope = 2;
-  displayName = "Mk. 19 40mm";
-  ammo = "G_20mm_HE";
-  count = 48;
-  initSpeed = 240;
-  maxLeadSpeed = 100;
-  nameSound = "grenadelauncher";
-};
